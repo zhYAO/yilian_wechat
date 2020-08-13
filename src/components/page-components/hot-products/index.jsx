16f920205a03209f090f1bfba37b308d05fd0a77
zhaoyao <359057276@qq.com>
2020-08-13 23:31:57 +0800

    style: 首页热门产品样式修改

--- src/components/page-components/hot-products/index.jsx
+++ src/components/page-components/hot-products/index.jsx
@@ -26,6 +26,7 @@ const HotProducts = props => {
               onClick={() => handleClick(item.id)}
             >
               <Image src={item.imgPath} mode="aspectFill" />
+              <View className="hot__name">{item.companyName}</View>
             </View>
           )
         })}
