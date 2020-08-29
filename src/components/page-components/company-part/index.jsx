2b71c3d57481841d2e6269d621f02340ca6859e6
zhaoyao <359057276@qq.com>
2020-08-29 22:23:03 +0800

    fix

--- src/components/page-components/company-part/index.jsx
+++ src/components/page-components/company-part/index.jsx
@@ -4,7 +4,6 @@ import CompanyCard from '@components/page-components/company-card'
 import CustomNavigator from '@components/page-components/custom-navigator'
 import { navigateTo } from '@crossplatform/apiservice/navigate'
 import pagejumplist from '@configuration/pagejumplist.json'
-import StickyWrap from '@components/page-components/sticky-wrap'
 
 const CompanyPart = props => {
   const { title, extraText, cardList } = props
@@ -17,9 +16,7 @@ const CompanyPart = props => {
 
   return (
     <View>
-      <StickyWrap height={40} fixTop={40}>
-        <CustomNavigator title={title} extraText={extraText} rightClick={rightClick} />
-      </StickyWrap>
+      <CustomNavigator title={title} extraText={extraText} rightClick={rightClick} />
       {cardList.map((item, index) => {
         if (index < 3) {
           return <CompanyCard key={item.id} data={item} />
