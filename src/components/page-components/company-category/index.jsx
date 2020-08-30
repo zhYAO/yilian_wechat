1773180e3e7b24440ebe9757bb49db9b5c927070
zhaoyao <359057276@qq.com>
2020-08-30 12:50:08 +0800

    perf: 公司分类排序优化

--- src/components/page-components/company-category/index.jsx
+++ src/components/page-components/company-category/index.jsx
@@ -1,11 +1,16 @@
-import Taro from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import Taro, { useState, useEffect } from '@tarojs/taro'
+import { View, Text, Swiper, SwiperItem } from '@tarojs/components'
 import { navigateTo } from '@crossplatform/apiservice/navigate'
 import pagejumplist from '@configuration/pagejumplist.json'
 import './index.less'
 
 const CompanyCategory = props => {
-  const { list } = props
+  const { list = [] } = props
+
+  const [listLenthArr, setListLenthArr] = useState([])
+  useEffect(() => {
+    setListLenthArr(new Array(Math.ceil(list.length / 8)).fill(''))
+  }, [list])
 
   const handleClick = (id, label) => {
     navigateTo({
@@ -15,24 +20,32 @@ const CompanyCategory = props => {
 
   return (
     <View className="container">
-      <View className="container__grid">
-        {list.map((item, index) => {
-          if (index < 8) {
+      {/* <View className="container__grid"> */}
+      <Swiper className="container__grid" circular>
+        {listLenthArr.length > 0 &&
+          listLenthArr.map((itemArr, indexArr) => {
+            const newList = list.slice(8 * indexArr, 8 * (indexArr + 1))
             return (
-              <View
-                className="container__grid__item"
-                key={item.id}
-                onClick={() => {
-                  handleClick(item.labelId, item.label)
-                }}
-              >
-                <Image className="item__icon" src={item.imgPath} />
-                <Text className="item__title">{item.label}</Text>
-              </View>
+              <SwiperItem key={indexArr}>
+                {newList.map(item => {
+                  return (
+                    <View
+                      className="container__grid__item"
+                      key={item.id}
+                      onClick={() => {
+                        handleClick(item.labelId, item.label)
+                      }}
+                    >
+                      <Image className="item__icon" src={item.imgPath} />
+                      <Text className="item__title">{item.label}</Text>
+                    </View>
+                  )
+                })}
+              </SwiperItem>
             )
-          }
-        })}
-      </View>
+          })}
+      </Swiper>
+      {/* </View> */}
     </View>
   )
 }
